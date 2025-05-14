ccf5819a24e7817db1a208c0c10da187012768eb
5to Bachillerato A <5to Bachillerato A@DESKTOP-MR3I3QR>
2025-05-14 12:26:58 -0600

    Agregar archivos de proyecto.

---
+++ Inicio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Repaso_2do_Bloque_JS
+{
+    public partial class frmInicio : Form
+    {
+        public frmInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NumAleatorio repaso1 = new NumAleatorio();
+            repaso1.Show();
+
+        }
+    }
+}
---
+++ NumAleatorio.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Repaso_2do_Bloque_JS
+{
+    public partial class NumAleatorio : Form
+    {
+        int aleatorio = 0;
+        Random rand = new Random();
+        public NumAleatorio()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtNummax.Text, out int nunmax))
+            {
+
+                if (nunmax < 1)
+                {
+                    MessageBox.Show("Ingrese un numero positivo");
+                }
+                else
+                {
+                    aleatorio = rand.Next(1, nunmax);
+                    MessageBox.Show($"Numero:{aleatorio}");
+                }
+            }
+
+
+            else
+            {
+                MessageBox.Show("Ingrese un numero a");
+            }
+        }
+    }
+}
